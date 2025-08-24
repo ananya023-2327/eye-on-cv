3006bce14c7857a9203a842d238fd368c4fbef69
Ananya Patni <patni.ananya.2531@gmail.com>
2025-08-24 12:38:38 +0530

    Update detect_contours.py

--- detect_contours.py
+++ detect_contours.py
@@ -1,14 +1,15 @@
 import cv2
 
-img_path = r" C:\Users\misht\OneDrive\Pictures\Saved Pictures\snoopy2.jpg" 
+img_path = "...\snoopy2.jpg" 
 img = cv2.imread(img_path)
 font = cv2.FONT_HERSHEY_SIMPLEX
 
-cv2.putText(img,"snoopyyyy", (10,100), font, 1,(0,0,255),1,cv2.LINE_AA)
+cv2.putText(img,"snoopy!", (10,100), font, 1,(0,0,255),1,cv2.LINE_AA)
 cv2.rectangle(img, (20, 200), (150, 300), (255, 0, 0), 2)
 cv2.circle(img, (100, 100), 50, (0, 255, 0), 3)
 cv2.waitKey(10)
 cv2.destroyAllWindows()
+
 
 
 
