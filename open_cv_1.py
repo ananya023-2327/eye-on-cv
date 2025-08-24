f60762ff4a4555e33d96ecde60d091ecd630c699
Ananya Patni <patni.ananya.2531@gmail.com>
2025-08-24 12:39:09 +0530

    Update open_cv_1.py

--- open_cv_1.py
+++ open_cv_1.py
@@ -4,20 +4,17 @@ print(cv2.__version__)
 
 cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 
-# Optionally set width and height
-# cap.set(3, 640)
-# cap.set(4, 480)
-
 while True:
     ret, img = cap.read()
     if not ret:
         print("Failed to grab frame")
         break
 
-    cv2.imshow("Webcam", img)  # Correct function is cv2.imshow (not cv2_imshow)
+    cv2.imshow("webcam", img) 
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
 cap.release()
 cv2.destroyAllWindows()
+
